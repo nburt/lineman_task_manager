40a1e621db9ff9e8b996dd50a4885ed6518a47d6
Nathanael Burt <nathanael.burt@gmail.com>
2014-08-27 09:37:26 -0600

    create simple rails tasks api

--- Gemfile
+++ Gemfile
@@ -11,3 +11,7 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'spring', group: :development
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0.2'
+end
---
+++ app/controllers/api/tasks_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  class TasksController < ApplicationController
+
+    def index
+      tasks = Task.all
+      render json: tasks
+    end
+
+    def create
+      task = Task.create!(name: params[:name], description: params[:description])
+      render json: task
+    end
+
+    def update
+      task = Task.find(params[:id])
+      task.update_attributes!(name: params[:name], description: params[:description])
+      render json: task
+    end
+
+    def destroy
+      task = Task.find(params[:id])
+      task.destroy
+      render json: task
+    end
+
+  end
+end
